466000d86e945fd390f355d156e3f60b44c9ee6e
DESKTOP-1BM1O1C\leemingyu <skystar9927@naver.com>
2023-07-20 21:57:16 +0900

    no message

---
+++ main.js
@@ -0,0 +1,7 @@
+function appendNumber() {
+    const element = document.getElementById('phonenumber');
+    const content = 
+          document.createTextNode("AppendChild");
+    
+    element.appendChild(content);
+  } 
